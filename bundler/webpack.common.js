3108dc473e566d3796690548e7d58099546727ab
zeriong <jaeryong95@gmail.com>
2023-11-21 01:05:26 +0900

    projects "웹개발 기초 학습" 내용 추가 시작

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -46,7 +46,7 @@ module.exports = {
 		}),
 		// origin 그대로 복사할 파일을 지정
 		new CopyWebpackPlugin({
-			// 빌드 시 dist 폴더 자동 생성(patterns 경로에 해당 파일이 없으면 에러발생)
+			// 빌드 시 public 폴더에 자동 생성(patterns 경로에 해당 파일이 없으면 에러발생)
 			patterns: [
 				{ from: path.resolve(__dirname, '../static') },
 				{ from: path.resolve(__dirname, '../src/style.css') },
