51dac9d5fe87e5f0cc3577c41de5d60ea163f530
zeriong <jaeryong95@gmail.com>
2023-11-27 00:28:07 +0900

    world 일부구조 변경

--- src/app/world/World.js
+++ src/app/world/World.js
@@ -2,21 +2,32 @@ import Doll from './Doll';
 import Room from './Room';
 import Fonts from './Fonts';
 import Environment from './Environment';
+import Lights from "./Lights";
+import GuestBook from "./GuestBook";
 
 export default class World {
+
+    // 싱글톤 패턴 적용
+    static getInstance() {
+        // 인스턴스가 없다면 새로 생성하고 있다면 인스턴스 반환
+        if (!World.instance) new World();
+        return World.instance;
+    }
+
     constructor() {
-        // positions에 등록
+        // 생성자에서 인스턴스 정의
+        World.instance = this;
+    }
+
+    init() {
+        this.lights = new Lights();
         this.room = new Room();
-        this.projectsPosition = this.room.projectsPosition;
-        this.historyPosition = this.room.roadMapPosition;
-        this.skillsPosition = this.room.skillsPosition;
-        this.posterPosition = this.room.posterPosition;
-
-        this.aboutMe = new Doll();
-        this.aboutMePosition = this.aboutMe.position;
-
+        this.doll = new Doll();
         this.fonts = new Fonts();
         this.environment = new Environment();
+        this.guestBook = new GuestBook();
+
+        return World.instance;
     }
 
     update() {
