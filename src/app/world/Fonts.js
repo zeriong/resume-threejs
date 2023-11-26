3772e33a5dc31586c5b7f71118784f0d0116ce96
zeriong <jaeryong95@gmail.com>
2023-11-26 21:58:47 +0900

    Fonts 네이밍, 구조 일부 수정

--- src/app/world/Fonts.js
+++ src/app/world/Fonts.js
@@ -4,40 +4,40 @@ import {TextGeometry} from 'three/addons/geometries/TextGeometry';
 
 export default class Fonts {
     constructor() {
-        const app = Application.getInstance();
-        this.intersectsMeshes = app.intersectsMeshes; // 레이캐스터 감지되는 매쉬를 담은 배열
-        this.loader = app.loader;
-        this.scene = app.scene;
-        this.nameGroup = new THREE.Group();  // zeriong, engineer 판넬 그룹
-        this.menuGroup = new THREE.Group();  // 메뉴 판넬 그룹
-        this.totalGroup = new THREE.Group();  // 전체 그룹
-
-        this.fontsLoad();
+        this.init();
     }
 
-    fontsLoad() {
+    init() {
+        const app = Application.getInstance()
+        const raycaster = app.raycaster
+        const loader = app.loader;
+
+        const authorGroup = new THREE.Group();  // zeriong, engineer 판넬 그룹
+        const menuGroup = new THREE.Group();  // 메뉴 판넬 그룹
+        const totalGroup = new THREE.Group();  // 전체 그룹
+
         const fixPos = { y: 0.089, x: 0.55 } // position: (y = +), (z = -)
 
         const clickAreaGeometry = new THREE.PlaneGeometry(1.3, 0.3, 1, 1);
-        const clickAreaMaterial = new THREE.MeshStandardMaterial({ opacity: 0, transparent: true });
+        const clickAreaMaterial = new THREE.MeshBasicMaterial({ opacity: 0, transparent: true });
 
         // load bold font
-        this.loader.fontLoader.load('/fontjson/Pretendard_Bold.json', (font) => {
+        loader.fontLoader.load('/assets/fontJson/Pretendard_Bold.json', (font) => {
             // bold mesh list
             [
-                { name: 'name', y: 0.45, text: 'ZERIONG' },
-                { name: 'aboutMe', y: 3, text: 'About Me' },
-                { name: 'projects', y: 2.5, text: 'Projects' },
-                { name: 'history', y: 2, text: 'history' },
-                { name: 'skills', y: 1.5, text: 'Skills' },
-                { name: 'github', y: 1, text: 'Github' },
-                { name: 'blog', y: 0.5, text: 'Blog' },
+                { text: 'ZERIONG', objName: 'name', y: 0.45 },
+                { text: 'About Me', objName: 'aboutMe_menu', y: 3 },
+                { text: 'Projects', objName: 'projects_menu', y: 2.5 },
+                { text: 'Skills', objName: 'skills_menu', y: 2 },
+                { text: 'Roadmap', objName: 'roadmap_menu', y: 1.5 },
+                { text: 'Github', objName: 'github_menu', y: 1 },
+                { text: 'Blog', objName: 'blog_menu', y: 0.5 },
             ]
                 .forEach((val) => {
                     const position = new THREE.Vector3();
                     const geometry = new TextGeometry(val.text, {
                         font: font,
-                        size: val.name !== 'name' ? 0.18 : 0.32,
+                        size: val.objName !== 'name' ? 0.18 : 0.32,
                         height: 0.001,
                         curveSegments: 5,
                         bevelSegments: 5,
@@ -48,41 +48,41 @@ export default class Fonts {
                     mesh.castShadow = true;
 
                     // 메뉴가 아닌 mesh는 그룹추가 후 foreach를 빠져나옴 (추가할 이벤트 따로 x)
-                    if (val.name === 'name') return this.nameGroup.add(mesh);
+                    if (val.objName === 'name') return authorGroup.add(mesh);
 
-                    mesh.name = val.name;
+                    mesh.name = val.objName;
                     mesh.getWorldPosition(position);
 
                     const clickAreaMesh = new THREE.Mesh(clickAreaGeometry, clickAreaMaterial);
                     clickAreaMesh.position.set(position.x + fixPos.x, position.y + fixPos.y, 0);
-                    clickAreaMesh.name = val.name;
+                    clickAreaMesh.name = val.objName;
 
-                    this.menuGroup.add(mesh, clickAreaMesh);
-                    this.intersectsMeshes.push(mesh, clickAreaMesh);
+                    menuGroup.add(mesh, clickAreaMesh);
+                    raycaster.targetMeshes.push(mesh, clickAreaMesh);
                 });
 
             // load regular font
-            this.loader.fontLoader.load('/fontjson/Pretendard_Regular.json', (font) => {
+            loader.fontLoader.load('/assets/fontJson/Pretendard_Regular.json', (font) => {
                 const geometry = new TextGeometry('Frontend Engineer', { font: font, size: 0.165, height: 0.001, curveSegments: 5, bevelSegments: 5 })
                 const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
                 const mesh = new THREE.Mesh(geometry, material);
 
                 mesh.position.y = 0.2;
                 mesh.castShadow = true;
-                this.nameGroup.add(mesh);
+                authorGroup.add(mesh);
             });
         });
 
         // 그룹 단위로 포지션 지정
-        this.nameGroup.position.z = 2.75;
+        authorGroup.position.z = 2.75;
 
-        this.menuGroup.position.x = -3.4;
-        this.menuGroup.position.z = -1.9;
+        menuGroup.position.x = -3.4;
+        menuGroup.position.z = -1.9;
 
-        this.totalGroup.add(this.menuGroup, this.nameGroup);
-        this.totalGroup.position.x = 1;
-        this.totalGroup.position.z = 2;
+        totalGroup.add(menuGroup, authorGroup);
+        totalGroup.position.x = 1;
+        totalGroup.position.z = 2;
 
-        this.scene.add(this.totalGroup);
+        app.scene.add(totalGroup);
     }
 }
